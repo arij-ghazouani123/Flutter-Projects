294b1f0c7d410389ba8dbfd934bdc2ef79733815
Arij Ghazouani <arijghazouani123@gmail.com>
2022-11-26 00:14:13 +0100

    first commit

--- lib/Screens/inscription.dart
+++ lib/Screens/inscription.dart
@@ -1,126 +1,75 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:project1/Screens/signup.dart';
 
-class Inscription extends StatelessWidget {
-  const Inscription({Key? key}) : super(key: key);
+class Signup extends StatelessWidget {
+  const Signup({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Inscription",
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
+        title: const Text("Inscription"),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: ListView(
+      body: ListView(
+        children: [
+          Container(
+              width: double.infinity,
+              margin: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+              child: Image.asset("assets/images/minecraft.jpg",
+                  width: 460, height: 215)),
+          Container(
+            margin: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+            child: const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: "Username"),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+            child: const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: "Email"),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+            child: const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: "Mot de passe"),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+            child: const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Année de naissance"),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(10, 0, 10, 20),
+            child: const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Adresse de facturation"),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset(
-                "assets/dmc5.jpg",
-                width: 400,
+              ElevatedButton(
+                child: const Text("S'inscrire"),
+                onPressed: () {},
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20.0),
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Username',
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                      height: 12.0,
-                      //Card
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Email',
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                      height: 12.0,
-                      //Card
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Mot de passe',
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                      height: 12.0,
-                      //Card
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Anné de naissance',
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                      height: 12.0,
-                      //Card
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Adresse de facturation',
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 20.0),
-                      child: Row(
-                        children: [
-                          SizedBox(
-                            width: 60.0,
-                            height: 12.0,
-                            //Card
-                          ),
-                          ElevatedButton(
-                            child: Text('Sinscrire'),
-                            style: ElevatedButton.styleFrom(
-                              primary: Color.fromARGB(255, 229, 55, 7),
-                            ),
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => Signup()));
-                            },
-                          ),
-                          SizedBox(
-                            width: 40.0,
-                            height: 12.0,
-                            //Card
-                          ),
-                          ElevatedButton(
-                            child: Text('Annuler'),
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.blue,
-                            ),
-                            onPressed: () {},
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+              const SizedBox(
+                width: 20,
+              ),
+              ElevatedButton(
+                child: const Text("Annuler"),
+                onPressed: () {},
               )
             ],
-          ),
-        ),
+          )
+        ],
       ),
     );
   }
